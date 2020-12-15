4c8a899a522678dd459b583202ac864ebd25e7c7
Kingsley Enoka <kuenoka@mix.wvu.edu>
2020-12-15 15:10:15 -0600

    Initial Commit

---
+++ Secrets/Secrets.h
@@ -0,0 +1,18 @@
+//
+//  Secrets.h
+//  Secrets
+//
+//  Created by Kingsley Enoka on 12/15/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Secrets.
+FOUNDATION_EXPORT double SecretsVersionNumber;
+
+//! Project version string for Secrets.
+FOUNDATION_EXPORT const unsigned char SecretsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Secrets/PublicHeader.h>
+
+
