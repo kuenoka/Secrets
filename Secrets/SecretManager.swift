71351014667d605395040f226b3e9c80fa68f2a8
Kingsley Enoka <kuenoka@mix.wvu.edu>
2021-02-06 21:52:03 -0600

    first commit

---
+++ Secrets/SecretManager.swift
@@ -0,0 +1,8 @@
+//
+//  SecretManager.swift
+//  Secrets
+//
+//  Created by Kingsley Enoka on 12/15/20.
+//
+
+import Foundation
---
+++ SecretsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecretsDemo
+//
+//  Created by Kingsley Enoka on 12/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
